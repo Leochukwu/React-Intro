a890dea70f8e573eaacd95fb791f850fe38da4f2
Chukwu <chukwugodwinc@gmail.com>
2023-12-14 09:41:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
 import React from 'react'
-import Navbar from './Component/Navbar/Navbar'
-import Slider from './Component/SliderImage/Slider'
-import Footer from './Component/Footer/Footer'
-import Form from './Component/RegForm/Form'
-import Card from './Component/Card/Card'
 
-const App = () => {
-  return (
-    <div>
-      <Navbar/>
-      <Slider/>
-      <Form/>
-      <Card/>
-      <Footer/>
-
-    </div>
-  )
+function App(){
+  return <div className='App'>Hello</div>
 }
 
-export default App
+export default App;
